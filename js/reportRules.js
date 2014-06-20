7abf9c784125154ec82f25ab488f798e8061146f
tomsitter <thomas.sitter@gmail.com>
2014-06-20 10:21:41 -0400

    Added appropriate license information to source files

--- js/reportRules.js
+++ js/reportRules.js
@@ -1,3 +1,18 @@
+/*
+	Chronic Disease Report Generator - Web based reports on quality of care standards
+    Copyright (C) 2014  Tom Sitter - Hamilton Family Health Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
 var reportRules =  (function(){
 	// Default comparison values for diabetic measures, based on Clinical Practice Guidelines and what we are asked to tracked in the report generator.
 	// NOTE: These are the DEFAULT values for comparison. There will be a settings menu to allow the user to modify these comparison values based on 
--- js/reportViewer.js
+++ js/reportViewer.js
@@ -1,3 +1,18 @@
+/*
+	Chronic Disease Report Generator - Web based reports on quality of care standards
+    Copyright (C) 2014  Tom Sitter - Hamilton Family Health Team
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
 var reportViewer = (function() {
 
 	var g_canvas = d3.select("#canvasContainer").select("#canvasSVG");
